ab347c9de428ed5b36529db199d9f79c34fc514a
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 15:43:35 -0300

    feat: log metrics w/ mlflow

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,12 +1,14 @@
 import mlflow
 from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from ucimlrepo import fetch_ucirepo
+
 from src.service.config.settings import settings
+
 
 def get_client():
     mlflow.set_tracking_uri(str(settings.MODEL_FILE_PATH))
@@ -15,6 +17,22 @@ def get_client():
     )
     return client
     
+def get_metrics(y_true, y_preds):
+    
+    accuracy = accuracy_score(y_true, y_preds)
+    f1 = f1_score(y_true, y_preds, average='weighted')
+    precision = precision_score(y_true, y_preds, average = 'weighted')
+    recall = recall_score(y_true, y_preds, average = 'weighted')
+
+    metrics = {
+        'accuracy': accuracy,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+    return metrics
+
 def main():
     client = get_client()
     iris_experiment = mlflow.set_experiment("IRIS")
@@ -40,7 +58,9 @@ def main():
 
     # MODEL EVALUATION
     preds = model.predict(X_test)
-    f1_score(y_test, preds, average='weighted')    
+    with mlflow.start_run() as run:
+        metrics = get_metrics(y_test, preds)
+        mlflow.log_metrics(metrics)
 
 if __name__ == '__main__':
     main()
