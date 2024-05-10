59cd48fb21f0151b27b82cd061d6b07b3fac161e
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 13:53:17 -0300

    chore: write docstring

--- src/service/use_cases/prediction.py
+++ src/service/use_cases/prediction.py
@@ -4,12 +4,33 @@ from src.service.ports.repositories import BaseRepository
 
 class PredictionUseCase:
     def __init__(self,
-                 model_repository: BaseRepository) -> None:
+                 model_repository: BaseRepository):
+        """Initialize the PredictionUseCase with a model repository.
+
+        Parameters
+        ----------
+        model_repository : BaseRepository
+            The repository containing the ML models.
+        """
         self.model_repository = model_repository
 
     def predict(self,
                 model_uid: str,
                 input: typing.Any) -> typing.Any:
+        """Make a prediction using the specified model.
+
+        Parameters
+        ----------
+        model_uid : str
+            The unique identifier of the model to use for prediction.
+        input : typing.Any
+            The input data for making the prediction.
+
+        Returns
+        -------
+        typing.Any
+            The prediction result.
+        """
         model = self.model_repository.get(model_uid)
-        ans = model.predict(input)
-        return ans
+        result = model.predict(input)
+        return result
