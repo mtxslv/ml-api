3b64a3e941194e50537693ffa0219a7d3d185c86
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 18:52:48 -0300

    test: iris data list dumping works

--- tests/domain/test_objects.py
+++ tests/domain/test_objects.py
@@ -30,3 +30,14 @@ def test_iris_data_values():
     assert iris.petal_width == 0.5
     assert iris.sepal_length == 5.1
     assert iris.sepal_width == 3.5
+
+def test_iris_data_to_list():
+    # Create an instance of IrisData
+    iris = IrisData(
+        petal_length=1.5,
+        petal_width=0.5,
+        sepal_length=5.1,
+        sepal_width=3.5
+    )
+
+    assert iris.to_2D_list() == [[5.1, 3.5, 1.5, 0.5]]
