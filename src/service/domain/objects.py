75832f0a852be26875641053c0cbaf41cab696e1
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 18:52:37 -0300

    feat: define method to dump Iris Data

--- src/service/domain/objects.py
+++ src/service/domain/objects.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from pydantic import BaseModel, Field
+
 
 class IrisData(BaseModel):
     """ Iris flowers information. """
@@ -6,3 +9,21 @@ class IrisData(BaseModel):
     petal_width: float = Field(..., description="Width of the petal in centimeters")
     sepal_length: float = Field(..., description="Length of the sepal in centimeters")
     sepal_width: float = Field(..., description="Width of the sepal in centimeters")
+
+    def to_2D_list(self) -> List[List[float]]:
+        """Return data following experiment feature order.
+        That is: sepal length, sepal width, petal length, and petal width.
+
+        Returns
+        -------
+        List[List[float]]
+            Iris Data following experiment feature order.
+        """
+        return [
+            [
+                self.sepal_length,
+                self.sepal_width,                
+                self.petal_length,
+                self.petal_width,
+            ]
+        ]
