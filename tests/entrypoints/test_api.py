0cd5ee4cf9e9b0de5f7e23ac7a89651a8a64d3c1
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 19:04:43 -0300

    test: predict with real model ;
    also fix test with fake model

--- tests/entrypoints/test_api.py
+++ tests/entrypoints/test_api.py
@@ -18,7 +18,7 @@ async def test_healthcheck(client):
 @pytest.mark.asyncio
 async def test_predict_fake_model(client):
     # Arrange
-    val_to_return = 'flower'
+    val_to_return = ['flower'] # model return an array
     fake_repo = FakeRepo([FakeModel(val_to_return)])
 
     experiment = 'my-wonderful-experiment'
@@ -28,9 +28,27 @@ async def test_predict_fake_model(client):
     sep_wid = 3
     pet_wid = 4
     url_string = f'/predict/{experiment}/{run}?sep_len={sep_len}&pet_len={pet_len}&sep_wid={sep_wid}&pet_wid={pet_wid}'
-    expected_response = {'model-response': val_to_return}
+    expected_response = {'model-response': val_to_return[0]}
     with app.container.model_repo.override(fake_repo):       
         response = await client.get(url_string)
+        assert response.status_code == 200
         response = response.json()
         logger.debug(response)
         assert response == expected_response
+
+@pytest.mark.asyncio
+async def test_predict(client):
+    experiment = 'IRIS'
+    run = 'unequaled-turtle-71'
+    sep_len = 5.8
+    pet_len = 2.8	
+    sep_wid = 5.1	
+    pet_wid = 2.4
+    url_string = f'/predict/{experiment}/{run}?sep_len={sep_len}&pet_len={pet_len}&sep_wid={sep_wid}&pet_wid={pet_wid}'
+    expected_response = {'model-response': 'Iris-virginica'}
+    
+    response = await client.get(url_string)
+    assert response.status_code == 200
+    response = response.json()
+    logger.debug(response)
+    assert response == expected_response
