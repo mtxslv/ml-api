35871a5d7bc1815843b6edf2dc4c3cce14909a52
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 16:51:32 -0300

    feat: define MlflowLocalRepository

---
+++ src/service/adapters/repositories.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import mlflow
+from mlflow.client import MlflowClient
+
+from src.service.ports.repositories import BaseRepository
+
+
+class MlflowLocalRepository(BaseRepository):
+    def __init__(self, uri: Path) -> None:
+        mlflow.set_tracking_uri(str(uri))
+        self.client = MlflowClient(
+            tracking_uri=str(uri)
+        )
+        
+    def get(self,
+            experiment_name,
+            model_name):
+        # Get IRIS experiment
+        experiments = self.client.get_experiment_by_name(experiment_name) # returns a list of mlflow.entities.Experiment
+
+        # Now get its run
+        runs = mlflow.search_runs(experiments.experiment_id)
+
+        a_run = runs[runs['tags.mlflow.runName'] == model_name]
+
+        if a_run.shape[0] == 0:
+            # raise RunNotFound 
+            pass
+        else:
+            model_uri = a_run.artifact_uri.to_list()[0]
+            model = mlflow.sklearn.load_model(
+                f'{model_uri}/model'
+            )
+            return model
