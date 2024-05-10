b0046dd61be2c677cdfff09f622770642c5d0ccc
Mateus Assis <mateus.d.assis.silva@gmail.com>
2024-05-10 18:56:12 -0300

    feat: pass endpoint inputs to pred use case and return answer

--- src/service/entrypoints/routers/routes.py
+++ src/service/entrypoints/routers/routes.py
@@ -1,12 +1,11 @@
 from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends, responses, status
 
-from fastapi import (APIRouter, Depends, Header, HTTPException, Query,
-                     responses, status)
-
-from src.service.use_cases.prediction import PredictionUseCase
-from src.service.config.settings import settings
+from src.service.domain.objects import IrisData
+from src.service.config.logger import get_logger
 
 router = APIRouter()
+logger = get_logger()
 
 @router.get('/healthcheck')
 def healthcheck():
@@ -15,16 +14,30 @@ def healthcheck():
         content = {'message': 'API is running!'}
     )
 
-@router.get('/predict/{experiment_name}/{model_name}')
+@router.get('/predict/{experiment_name}/{run_name}')
 @inject
 def predict(experiment_name : str,
-            model_name : str, 
+            run_name : str, 
+            sep_len : float,            
+            pet_len : float,            
+            sep_wid : float,
+            pet_wid : float,
             predict_use_case = Depends(Provide["prediction_use_case"])):
 
-    # preds = predict_use_case.predict()
-    
+    iris = IrisData(
+        petal_length = pet_len,
+        petal_width = pet_wid,
+        sepal_length = sep_len,
+        sepal_width = sep_wid
+    )
+
+    preds = predict_use_case.predict(
+        experiment_name,
+        run_name,
+        iris.to_2D_list()
+    )
     
     return responses.JSONResponse(
         status_code = status.HTTP_200_OK,
-        content = {} #preds.model_dump(mode="json")
+        content = {'model-response':preds} 
     ) 
